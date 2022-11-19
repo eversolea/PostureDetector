655c7ebef6e9b6c00cd5e84562ca44410e608525
eversolea <netranger9@gmail.com>
2022-11-19 16:12:14 -0700

    Add files via upload

---
+++ detectorScript/linuxHelpers/startup1.sh
@@ -0,0 +1,3 @@
+sleep 1m
+cd /home/perusal-dev/Documents/PostureDetector
+OPENBLAS_CORETYPE=ARMV8 python3 infer-simple.py
---
+++ detectorScript/linuxHelpers/startup2.sh
@@ -0,0 +1,1 @@
+sudo docker run --net=host --gpus all roboflow/inference-server:jetson
